2bf8f5e0a4641afbeba6d4db9c464796571ab540
breno-pinheir0 <breno.oliveira@hotmail.com.br>
2023-06-05 17:59:32 -0300

    .

--- Codigos-Sites/JavaScript/script.js
+++ Codigos-Sites/JavaScript/script.js
@@ -1,5 +1,5 @@
-var elem = document.getElementById('btnHello'); // Por meio da função "getElementById, obtemos o elemento com id 'btnHello'"
-elem.addEventListener('click', function(){ // toda vez que o usuário clicar no botão, é executada a função que está dentro das chaves
+var elem = document.getElementById("btnHello"); // Por meio da função "getElementById", obtemos o elemento com id "btnHello"
+elem.addEventListener("click", function(){ // toda vez que o usuário clicar no botão, é executada a função que está dentro das chaves
     var divTela = document.getElementById("tela");
-    divTela.innerHTML = "Olá Mundo";
+    divTela.innerHTML = "Olá Mundo"; // adicionamos no HTML do elemento "tela" o escrito "Olá Mundo"
 }, false);
