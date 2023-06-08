941da1a4f64657fa16642689b103baa2a5b5e733
breno_pinheir0 <breno.oliveira@hotmail.com.br>
2023-06-08 18:16:10 -0300

    .

---
+++ Codigos-Sites/JSON/app.js
@@ -0,0 +1,31 @@
+var Cadastro = [{
+    "nome": "Rommel Carneiro",
+    "cidade": "Belo Horizonte",
+    "idade": 25,
+    "veiculos": [{
+        "marca": "Fiat",
+        "modelo": "Toro",
+        "ano": 2020,
+        "placa": "HMV-1010"
+    }]
+}, {
+    "nome": "Maria Tereza",
+    "cidade": "São Paulo",
+    "idade": 28,
+    "veiculos": []
+}, {
+    "nome": "Pedro Augusto Souza",
+    "cidade": "Rio de Janeiro",
+    "idade": 48,
+    "veiculos": [{
+        "marca": "Ford",
+        "modelo": "Territory",
+        "ano": 2021,
+        "placa": "XYZ-4321"
+    }, {
+        "marca": "Toyota",
+        "modelo": "Etyos",
+        "ano": 2019,
+        "placa": "XWY-1368"
+    }]
+}]
